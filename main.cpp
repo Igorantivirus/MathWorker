320a07cd6cde557150a9c1fa4c29404684916759
Игорь Антипов <igorantivirus@yandex.ru>
2025-06-06 01:17:47 +0700

    Создание стандартных алгебр

--- main.cpp
+++ main.cpp
@@ -8,7 +8,17 @@
 
 using namespace mathWorker;
 
-SignatureContext makeBaseContext()
+VariableContext generateBaseConstants()
+{
+	VariableContext context;
+	context["pi"]	= ValueNode(RealType(std::acos(-1))).clone();
+	context["e"]	= ValueNode(std::exp(1)).clone();
+	context["phi"]	= ValueNode(RealType(0.5l + std::sqrt(5) / 2.l)).clone();
+	context["i"]	= ValueNode(ComplexType(0, 1)).clone();
+	context["inf"]	= ValueNode(ComplexType(RealType(1.) / RealType(sin(0)), 0)).clone();
+	return context;
+}
+SignatureContext generateBaseSignature()
 {
 	SignatureContext context;
 
@@ -25,14 +35,6 @@ SignatureContext makeBaseContext()
 		[](const std::vector<MathNodeP>& params)->MathNodeP
 		{
 			ComplexType res = params[0]->getNumberForced() - params[1]->getNumberForced();
-			return std::make_unique<ValueNode>(res);
-		},0, SignatureType::operation
-	};
-	context["@"] =
-	{
-		[](const std::vector<MathNodeP>& params)->MathNodeP
-		{
-			ComplexType res = params[0]->getNumberForced() - params[1]->getNumberForced() * ComplexType(10);
 			return std::make_unique<ValueNode>(res);
 		},0, SignatureType::operation
 	};
@@ -60,19 +62,86 @@ SignatureContext makeBaseContext()
 			return std::make_unique<ValueNode>(res);
 		},2, SignatureType::operation
 	};
-	context["sqrt"] =
-	{
-		[](const std::vector<MathNodeP>& params)->MathNodeP
-		{
-			ComplexType res = std::sqrt(params[0]->getNumberForced());
-			return std::make_unique<ValueNode>(res);
-		},3, SignatureType::function
-	};
+
 	context["sin"] =
 	{
 		[](const std::vector<MathNodeP>& params)->MathNodeP
 		{
 			ComplexType res = std::sin(params[0]->getNumberForced());
+			return std::make_unique<ValueNode>(res);
+		},3, SignatureType::function
+	};
+	context["cos"] =
+	{
+		[](const std::vector<MathNodeP>& params)->MathNodeP
+		{
+			ComplexType res = std::cos(params[0]->getNumberForced());
+			return std::make_unique<ValueNode>(res);
+		},3, SignatureType::function
+	};
+	context["tg"] =
+	{
+		[](const std::vector<MathNodeP>& params)->MathNodeP
+		{
+			ComplexType res = std::tan(params[0]->getNumberForced());
+			return std::make_unique<ValueNode>(res);
+		},3, SignatureType::function
+	};
+	context["ctg"] =
+	{
+		[](const std::vector<MathNodeP>& params)->MathNodeP
+		{
+			ComplexType res = ComplexType(1) / std::tan(params[0]->getNumberForced());
+			return std::make_unique<ValueNode>(res);
+		},3, SignatureType::function
+	};
+
+	context["arcsin"] =
+	{
+		[](const std::vector<MathNodeP>& params)->MathNodeP
+		{
+			ComplexType res = std::asin(params[0]->getNumberForced());
+			return std::make_unique<ValueNode>(res);
+		},3, SignatureType::function
+	};
+	context["arccos"] =
+	{
+		[](const std::vector<MathNodeP>& params)->MathNodeP
+		{
+			ComplexType res = std::acos(params[0]->getNumberForced());
+			return std::make_unique<ValueNode>(res);
+		},3, SignatureType::function
+	};
+	context["arctg"] =
+	{
+		[](const std::vector<MathNodeP>& params)->MathNodeP
+		{
+			ComplexType res = std::atan(params[0]->getNumberForced());
+			return std::make_unique<ValueNode>(res);
+		},3, SignatureType::function
+	};
+	context["arcctg"] =
+	{
+		[](const std::vector<MathNodeP>& params)->MathNodeP
+		{
+			ComplexType res = ComplexType(std::acos(-1) / 2) - std::atan(params[0]->getNumberForced());
+			return std::make_unique<ValueNode>(res);
+		},3, SignatureType::function
+	};
+
+	context["ln"] =
+	{
+		[](const std::vector<MathNodeP>& params)->MathNodeP
+		{
+			ComplexType res = std::log(params[0]->getNumberForced());
+			return std::make_unique<ValueNode>(res);
+		},3, SignatureType::function
+	};
+	context["exp"] =
+	{
+		[](const std::vector<MathNodeP>& params)->MathNodeP
+		{
+			ComplexType res = std::exp(params[0]->getNumberForced());
 			return std::make_unique<ValueNode>(res);
 		},3, SignatureType::function
 	};
@@ -92,6 +161,15 @@ SignatureContext makeBaseContext()
 			return std::make_unique<ValueNode>(res);
 		},3, SignatureType::specialFunction
 	};
+	context["sqrt"] =
+	{
+		[](const std::vector<MathNodeP>& params)->MathNodeP
+		{
+			ComplexType res = std::sqrt(params[0]->getNumberForced());
+			return std::make_unique<ValueNode>(res);
+		},3, SignatureType::function
+	};
+
 	context["d"] =
 	{
 		[](const std::vector<MathNodeP>& params)->MathNodeP
@@ -108,10 +186,11 @@ SignatureContext makeBaseContext()
 			return std::make_unique<ValueNode>(res);
 		},4, SignatureType::unare
 	};
+
 	return context;
 }
 
-SignatureContext makeBoolCOntext()
+SignatureContext makeBoolContext()
 {
 	SignatureContext context;
 
@@ -203,18 +282,44 @@ std::ostream& operator<<(std::ostream& out, const std::vector<T>& v)
 	return out << '}';
 }
 
+std::string input(std::string s = {})
+{
+	std::cout << s;
+	std::getline(std::cin, s);
+	return s;
+}
+
 int main()
 {
-	SignatureContext context = makeBoolCOntext();
+	SignatureContext context = generateBaseSignature();
+	VariableContext constants = generateBaseConstants();
+
 	MathParser parse;
 	parse.setContext(&context);
 
-	std::string s = "(1&0)&(1|1!)<->1!";
+	/*std::string s;
+
+	while (true)
+	{
+		s = input("Enter: ");
+		MathNodeP ptr = parse.parse(s);
+		std::cout << ptr->toString() << '\n';
+		std::cout << ptr->replace(constants)->calculate(context)->toString() << '\n';
+	}*/
+
+
+	std::string s = "log2(8)^2";
+	//std::string s = "e^(2*i)-(cos(2)+i*sin(2))+e+exp(1)";
 
 	MathNodeP ptr = parse.parse(s);
 
 	std::cout << ptr->toString() << '\n';
-	std::cout << ptr->calculate(context)->toString() << '\n';
+	std::cout << ptr->replace(constants)->calculate(context)->toString() << '\n';
+
+	
+
+
+
 
 	//SignatureContext context = makeBaseContext();
 
