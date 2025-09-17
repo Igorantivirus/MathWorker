54b76fa4b6d57b69c15b11b53010c401590051c2
Игорь Антипов <igorantivirus@yandex.ru>
2025-09-17 21:45:20 +0700

    ParseUtils разделён на hpp и cpp

--- include/MathWorker/Parse/ParseUtils.hpp
+++ include/MathWorker/Parse/ParseUtils.hpp
@@ -3,29 +3,11 @@
 namespace mathWorker
 {
 
-	bool isLetter(const char c)
-	{
-		return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z');
-	}
-	bool isNumber(const char c)
-	{
-		return (c >= '0' && c <= '9') || c == '.';
-	}
-	bool isOpenBracket(const char c)
-	{
-		return c == '(';
-	}
-	bool isCloseBracket(const char c)
-	{
-		return c == ')';
-	}
-	bool isComma(const char c)
-	{
-		return c == ',';
-	}
-	bool isNone(const char c)
-	{
-		return !isNumber(c) && !isLetter(c) && !isOpenBracket(c) && !(c == ' ');
-	}
+	bool isLetter(const char c);
+	bool isNumber(const char c);
+	bool isOpenBracket(const char c);
+	bool isCloseBracket(const char c);
+	bool isComma(const char c);
+	bool isNone(const char c);
 
 }
---
+++ src/MathWorker/Parse/ParseUtils.cpp
@@ -0,0 +1,31 @@
+#pragma once
+
+namespace mathWorker
+{
+
+	bool isLetter(const char c)
+	{
+		return (c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z');
+	}
+	bool isNumber(const char c)
+	{
+		return (c >= '0' && c <= '9') || c == '.';
+	}
+	bool isOpenBracket(const char c)
+	{
+		return c == '(';
+	}
+	bool isCloseBracket(const char c)
+	{
+		return c == ')';
+	}
+	bool isComma(const char c)
+	{
+		return c == ',';
+	}
+	bool isNone(const char c)
+	{
+		return !isNumber(c) && !isLetter(c) && !isOpenBracket(c) && !(c == ' ');
+	}
+
+}
